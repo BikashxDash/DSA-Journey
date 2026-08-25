ac5bb23fc30aea48e200b9d1afa93dce24634eee
BIKASH DASH <bikashxdash@gmail.com>
2026-08-25 23:44:17 +0530

    Day 4 DSA Practice (Loops)

---
+++ src/Loops.java
@@ -0,0 +1,67 @@
+public class Loops {
+    static void main() {
+
+//        for (int i=1; i<=5; i++) {
+//            System.out.println(i);
+//        }
+
+//        Nested loop
+//        for (int i=1; i<=3; i++) {
+//            for (int j=1; j<=3; j++) {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i=1; i<=3; i++) {
+//            for (int j=1; j<=3; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
+//            }
+//        }
+
+//        Break & Continue
+//        for (int i=1; i <= 10; i++){
+//            if(i == 5) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+//        for (int i=1; i <= 10; i++){
+//            if(i == 5) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+//        While Loop
+//        int i = 1;
+//
+//        while(i <= 5){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        int i = 1;
+//
+//        while(i <= 2){
+//            int j = 1;
+//
+//            while(j <= 3){
+//                System.out.println("i=" + i + ", j=" + j);
+//                j++;
+//            }
+//            i++;
+//        }
+
+//        Do-While Loop
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while(i <= 5);
+
+    }
+}
