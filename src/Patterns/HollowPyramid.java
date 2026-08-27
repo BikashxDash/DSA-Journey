bddff255bff35e68b7f6e27a160bec3b758f3e78
BIKASH DASH <bikashxdash@gmail.com>
2026-08-27 19:53:30 +0530

    Day 6 DSA Practice (HollowPyramid printed)

--- src/Patterns/HollowPyramid.java
+++ src/Patterns/HollowPyramid.java
@@ -7,6 +7,7 @@ public class HollowPyramid {
         int n = 5;
 
         for (int row = 1; row <= n; row ++) {
+//            for each row -> variable cols
 //            spaces
             for (int col = 1; col <= (n-row); col++) {
                 System.out.print("  ");
