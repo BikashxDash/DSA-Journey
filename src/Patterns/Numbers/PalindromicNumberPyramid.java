6c30148ca1fb894be21d74f0ff6af51b2a41a6f5
BIKASH DASH <bikashxdash@gmail.com>
2026-09-11 19:47:08 +0530

    Day 8 DSA Practice (Palindromic Number Pyramid printed)

--- src/Patterns/Numbers/PalindromicNumberPyramid.java
+++ src/Patterns/Numbers/PalindromicNumberPyramid.java
@@ -19,7 +19,6 @@ public class PalindromicNumberPyramid {
             }
 
 //            part 2 numbers
-            int rowValue = row;
             int decRowValue = row-1;
             for (int col = 1; col <= row-1; col++) {
                 System.out.print(decRowValue + " ");
